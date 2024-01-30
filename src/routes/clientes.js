afb89814427d7613aa42328a6f6a3f695860dc43
Ricardo Mantilla <jrmantillap@utn.edu.ec>
2024-01-30 07:30:33 -0500

    first commit

--- src/routes/clientes.js
+++ src/routes/clientes.js
@@ -1,4 +1,4 @@
-const {Router} = require('express');
+const { Router } = require('express');
 const router = Router();
 const _ = require('underscore');
 
@@ -7,74 +7,65 @@ const { title } = require('process');
 console.log(clientes);
 
 
-router.get('/', (req, res) =>{
+router.get('/', (req, res) => {
     res.send(clientes);
 });
 
 /*nuevarama*/
-router.post('/', (req, res)=>{
-  
-    const {nombre, correo_electronico, telefono} = req.body
-    if(  nombre && correo_electronico && telefono) {
-        const id = clientes.length +1;
-        const newCliente = {...req.body,id};
+router.post('/', (req, res) => {
+
+    const { nombre, correo_electronico, telefono } = req.body
+    if (nombre && correo_electronico && telefono) {
+        const id = clientes.length + 1;
+        const newCliente = {...req.body, id };
         console.log(newCliente);
         clientes.push(newCliente);
         res.json(clientes)
-    }else {
-        res.status(500)({error: 'Ocurrio un error inesperado'});
+    } else {
+        res.status(500)({ error: 'Ocurrio un error inesperado' });
     }
- });
+});
 
 
- router.delete('/:id', (req, res)=>{
-    const{id}= req.params;
-    _.each(clientes, (cliente, i) =>{
-        if(cliente.id == id){
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    _.each(clientes, (cliente, i) => {
+        if (cliente.id == id) {
             cliente.splice(i, 1);
         }
-    } )
+    })
     res.send(clientes);
 });
 
 
 
 
-router.put('/:id', (req, res)=>{
-    const {id} = req.params
-    const {nombre, correo_electronico, telefono } = req.body
-    if(nombre && correo_electronico && telefono) {
-        _.each(clientes, (cliente, i) =>{
-            if(cliente.id == id){
+router.put('/:id', (req, res) => {
+    const { id } = req.params
+    const { nombre, correo_electronico, telefono } = req.body
+    if (nombre && correo_electronico && telefono) {
+        _.each(clientes, (cliente, i) => {
+            if (cliente.id == id) {
                 cliente.nombre = nombre;
                 cliente.correo_electronico = correo_electronico;
                 cliente.telefono = telefono;
             }
         });
         res.json(clientes);
-    }else{
-        res.status(500).json({error: 'Hubo un error'});
+    } else {
+        res.status(500).json({ error: 'Hubo un error' });
     }
 });
 
 
-module.exports= router;
+module.exports = router;
 
-/*
 
- 
 
- app.get('/routes/clientes/:id',  (req, res) => {
-     const cliente = clientes.find(c =>c.id === parseInt(req.params.id));
-     if (!cliente) return res.status(404).send('Cliente no encontrado');
-     else res.send(cliente)
- });
- 
- 
- 
-     clientes.push(cliente);
-     res.send(cliente);
- });
- 
- 
- */
+
+
+app.get('/routes/clientes/:id', (req, res) => {
+    const cliente = clientes.find(c => c.id === parseInt(req.params.id));
+    if (!cliente) return res.status(404).send('Cliente no encontrado');
+    else res.send(cliente)
+});
